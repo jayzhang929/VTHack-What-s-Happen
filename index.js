7afa6ee41db04675447a6a448e3eb90a5648ab28
Jay Zhang <jayzhang@Jays-MacBook-Air.local>
2015-02-06 23:32:25 -0500

    fundamental of monk works

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
+var db = require('monk')('localhost:27017/mydb');
+var users = db.get('users');
 
 //asking the front end to see the folder
 app.use(express.static(__dirname + '/css'));
@@ -11,18 +13,29 @@ app.get('/', function(req, res){
   res.sendfile('index.html');
   
 });
-
+app.get('/test', function(req,res){
+    users.find({}, function(err, data) {
+      console.log(data);
+    });
+    return  
+});
 app.get('/form', function(req, res) {
+    users.insert({name: req.query['username'], EventName: req.query['eventname'], Loc: req.query['location']});
     res.send('Username: ' + req.query['username'] + 
       'Eventname: ' + req.query['eventname'] + 
       'Location: ' + req.query['location']);
+
 });
 
 	
 io.on('connection', function(socket){
   //reading the file
   socket.on('newsRequest', function(data) {
-    socket.emit('news', "VTHack is going on now!");
+    users.find({}, function(err, data) {
+      console.log(data.length);
+      socket.emit('news', data);
+    });
+    
   });
   
 
