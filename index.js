1d61669b96db357e6cf0ec4fcde9858461f06413
Jay Zhang <jayzhang@Jays-MacBook-Air.local>
2015-02-07 09:58:12 -0500

    date sorted

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var io = require('socket.io')(http);
 var fs = require('fs');
 var db = require('monk')('localhost:27017/mydb');
 var users = db.get('users');
+var today = new Date();
+users.index({'DateField': 1}, {'DateField': { $gt: today }});
 
 //asking the front end to see the folder
 app.use(express.static(__dirname + '/css'));
@@ -20,10 +22,13 @@ app.get('/test', function(req,res){
     return  
 });
 app.get('/form', function(req, res) {
-    users.insert({name: req.query['username'], EventName: req.query['eventname'], Loc: req.query['location']});
-    res.send('Username: ' + req.query['username'] + 
-      'Eventname: ' + req.query['eventname'] + 
-      'Location: ' + req.query['location']);
+
+    var d = new Date(req.query['month'] + " " + req.query['date'] + ", " + "2015 " + req.query['time']);
+    users.insert({name: req.query['username'], EventName: req.query['eventname'], 
+                  Loc: req.query['location'], DateField: d});
+    //res.send('Username: ' + req.query['username'] + 
+      //'Eventname: ' + req.query['eventname'] + 
+      //'Location: ' + req.query['location']);
 
 });
 
@@ -31,8 +36,9 @@ app.get('/form', function(req, res) {
 io.on('connection', function(socket){
   //reading the file
   socket.on('newsRequest', function(data) {
-    users.find({}, function(err, data) {
-      console.log(data.length);
+    console.log(today);
+    users.find({ 'DateField' : { $gt: today } }, { sort: { 'DateField': 1 } }, function(err, data) {
+      console.log(data[0].DateField.getFullYear());
       socket.emit('news', data);
     });
     
